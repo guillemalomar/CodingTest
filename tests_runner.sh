d38f19e0107c4b11751291bb326a552f8f06ef6c
guillemnicolau <guillemnicolau@gmail.com>
2017-08-03 17:17:03 +0200

    Now it's correctly added

---
+++ tests_runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nosetests Problem1/tests/GivenTests.py
+
+nosetests Problem2/tests/PrinterTests.py
+nosetests Problem2/tests/SchedulerAppTests.py
+nosetests Problem2/tests/SchedulerTests.py
+
+nosetests Problem3/tests/MerchantToolTests.py
+nosetests Problem3/tests/ParserTests.py
